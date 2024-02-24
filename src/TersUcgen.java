c52e39e6d7b334236b50a121c8be0e7edb96f667
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-24 15:07:53 +0300

    Ters Üçgen Yapımı

--- src/TersUcgen.java
+++ src/TersUcgen.java
@@ -2,30 +2,17 @@ import java.util.Scanner;
 
 public class TersUcgen {
     public static void main(String[] args) {
-        int n  ;
-        //satır sayısını kullanıcığımızı belittik.
+        int n;
+        //satır sayısını kullanıcadan alıyoruz.
         Scanner scan = new Scanner(System.in);
         System.out.print("Satır Sayısınız giriniz: ");
         n = scan.nextInt();
-      //Kullanıcıdan aldığı değer kadar döngüyü çalıştırdık.
-        for (int i = n; i>=1 ; i--) {
-
-            //Boşluklar
-            for (int j = 0; j < n - i; j++) {
-                System.out.print(" ");
-            }
-
-            // Yıldızlar
+        //Kullanıcıdan aldığı değeri azaltarak çalıştırıyoruz
+        for (int i = n; i > 0; i--) {
             for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print("*");
             }
-
-
             System.out.println();
-
         }
-
     }
-
-
 }
