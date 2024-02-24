87209789db7258838a5b62dbba75bb02e495e652
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-24 14:45:10 +0300

    Yorum satırı eklendi

--- src/MaxveMinDegerBulma.java
+++ src/MaxveMinDegerBulma.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class MaxveMinDegerBulma {
     public static void main(String[] args) {
         int n, min = 0, max = 0, inpnumber;
+
+        //kullanıcıdan n kadar değer aldık
         Scanner scan = new Scanner(System.in);
         System.out.print("Kaç tane sayı gireceksiniz : ");
         n = scan.nextInt();
+
+        //n değerinden küçük ve eşit olana kadar artarak gidecek şekilde tanımladık
         for (int i = 1; i <= n; i++) {
             System.out.print(i + ". sayıyı giriniz : ");
             inpnumber = scan.nextInt();
@@ -13,8 +17,8 @@ public class MaxveMinDegerBulma {
                 min = inpnumber;
                 max = inpnumber;
             } else {
-                if (inpnumber > max) max = inpnumber;
-                if (inpnumber < min) min = inpnumber;
+                if (inpnumber > max) max = inpnumber; //en büyük değeri atadık
+                if (inpnumber < min) min = inpnumber; //en küçük değeri atadık
 
             }
         }
