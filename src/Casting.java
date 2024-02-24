88397d1fce310500b3ce28246a21a660bda33dbc
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-24 16:23:47 +0300

    Casting added

--- src/Casting.java
+++ src/Casting.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Casting {
     public static void main(String[] args) {
-        int numberint;
-        double numberdouble=0;
+        //Kullanıcıdan alacağımız değerleri tanımladık
+        int number1;
+        double number2 = 0;
+
+        //değerleri aldık
         Scanner scan = new Scanner(System.in);
         System.out.print("Bir tam sayı giriniz : ");
-        numberint=scan.nextInt();
+        number1 = scan.nextInt();
         System.out.print("Bir ondalıklı sayı giriniz : ");
-        numberdouble=scan.nextDouble();
+        number2 = scan.nextDouble();
 
-        numberint=(int)numberdouble;
-        System.out.println("Tam sayın artık double"+ numberdouble);
-
-        numberdouble=(double) numberint;
-        System.out.println("Ondalıklı sayın artık double"+ numberint);
-
-        System.out.println(numberint);
-
+        //double değeri int değerine dönüştürdük
+        int numberint = (int) number2;
+        System.out.println("Tam sayın artık double : " + numberint);
+        //int değeri double değere dönüştürdü
+        double numberdouble = (double) number1;
+        System.out.println("Ondalıklı sayın artık double : " + numberdouble);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int num1;
+        double num2;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        num1 = input.nextInt();
+        System.out.println("Enter a double: ");
+        num2 = input.nextDouble();
+        double num1_double = (double) num1;
+        System.out.println("whole number to decimal number: " + num1_double);
+        int num2_integer = (int) num2;
+        System.out.println("from decimal to integer: " + num2_integer);
 
         }
     }
