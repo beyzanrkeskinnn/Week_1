aeb06a65596d8c198dae89b3d2ff0bf342abbd23
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-24 13:24:41 +0300

    Fibonacci Serisi Eklendi

--- src/FibonacciSerisi.java
+++ src/FibonacciSerisi.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
+
 public class FibonacciSerisi {
     public static void main(String[] args) {
-        int inp, n1=0, n2=1, total = 0;
+        int inp, n1 = 0, n2 = 1, total = 0;
         Scanner scan = new Scanner(System.in);
         System.out.print("Bir sayı giriniz : ");
         inp = scan.nextInt();
-        System.out.println(inp+ " Girdiğiniz sayının fibonacci serisi: ");
+        System.out.println(inp + " Girdiğiniz sayının fibonacci serisi: ");
         for (int i = 1; i <= inp; i++) {
-            System.out.print(n1+ " ,");
-            total=n1+n2;
-            n1=n2;
-            n2=total;
+            System.out.print(n1 + " ,");
+            total = n1 + n2;
+            n1 = n2;
+            n2 = total;
         }
     }
-    }
+}
