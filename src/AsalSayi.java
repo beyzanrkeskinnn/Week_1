b9455ea01b7ca4d8954377f68feb2bf90326c2d1
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-24 12:27:37 +0300

    Asal Sayi ödevi eklendi

---
+++ src/AsalSayi.java
@@ -0,0 +1,21 @@
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        int sayac = 0;
+        for (int i = 2; i <= 100; i++) {
+            int kontrol = 0;
+            for (int k = 2; k < i; k++) {
+                if (i % k == 0) {
+                    kontrol = 1;
+                    break;
+                }
+            }
+
+            if (kontrol == 0) {
+                System.out.print(i + "\n");
+                sayac++;
+            }
+
+        }
+    }
+}
