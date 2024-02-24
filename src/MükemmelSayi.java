365c6ab58898770a1ff55c007319093afb6bc12c
Beyzanur KESKİN <beyzanrkeskinn94@gmail.com>
2024-02-24 14:56:10 +0300

    Yorum satırı eklendi

--- src/MükemmelSayi.java
+++ src/MükemmelSayi.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
+
 public class MükemmelSayi {
-public static void main(String[] args) {
-    int n, i, total=0;
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Bir sayı giriniz : ");
-    n = scan.nextInt();
+    public static void main(String[] args) {
+        int n, i, total = 0;
+        //kullanıcıdan n değeri alıyoruz
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        n = scan.nextInt();
 
-    for(i=1; i<n; i++){
-        if(n % i==0){
-            total+=i;
-
+        // kalansız bölen sayıları bulup topluyoruz
+        for (i = 1; i < n; i++) {
+            if (n % i == 0) {
+                total += i;
+            }
         }
 
-
+        //toplam sayıya eşitse ekrana çıktıyı veriyoruz
+        if (total == n) {
+            System.out.println(n + " Sayısı mükemmel sayıdır");
+        } else {
+            System.out.println(n + " sayısı mükemmel sayı değildir");
+        }
     }
-    if(total==n){
-        System.out.println(n+ " Sayısı mükemmel sayıdır");
-    }
-    else{
-        System.out.println(n + " sayısı mükemmel sayı değildir");
-    }
-
-}
 }
